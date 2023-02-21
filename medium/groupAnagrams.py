0078ad941ae4a78915fd75fa57a73a1c5c7ebc41
TrongHieuNg <110407360+TrongHieuNg@users.noreply.github.com>
2023-02-21 19:39:31 +1100

    Update groupAnagrams.py

--- medium/groupAnagrams.py
+++ medium/groupAnagrams.py
@@ -1,1 +1,20 @@
+"""
+Given an array of strings strs, group the anagrams together.
+
+Example 1:
+strs = ["eat","tea","tan","ate","nat","bat"]
+return [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+"""
+
+from collections import defaultdict
+
+def groupAnagrams(strs):
+    
+    dct = defaultdict(list)
+    
+    for word in strs:
+        dct(tuple(sorted(word))).append(word)
+        
+    return dct.values()
 
